45da04a989b75fd913ea802d5ab66b96dffd8363
ToastYwasTaken <franz@emoerike.de>
2023-10-06 15:41:34 +0200

    finished basic implementation
    - adjusted PlayerMovement.cs
          -> added accelleration modifier
          -> added separated variables for horizantal / vertical movement
          -> changed horizontal movement from AddForce to assigning rb.velocity
          -> implemented continous vertical movement
     - added Template to Prefabs for later PCG creation
    - adjusted sizes and values

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ using UnityEngine;
  * ----------------------------
  *  18.08.2022   FM  created
  *  14.09.2023   FM  removed unnecessary parts
+ *  06.10.2023   FM  added horizontal continous movement, adjusted values, added seperated acceleration and speed variables
  *  
  *****************************************************************************/
 public class PlayerController : MonoBehaviour
@@ -27,21 +28,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float RBMass = 50f;
     [SerializeField]
-    float PlayerMovementSpeed = 5f;
+    float SpeedVertical = 5f;
     [SerializeField]
-    float SpeedMultiplier = 10f;
+    float SpeedHorizontal = 5f;
     [SerializeField]
     float JumpForce = 200f;
+    //TODO: FineTune these values:
+    [SerializeField]
+    float AccellerationVertical = 0.1f;
+    [SerializeField]
+    float AccellerationHorizontal = 0.05f;
 
     [ReadOnly]
     public bool m_pIsGrounded;
-
 
     private Vector3 m_pSpawnPosition = new Vector3(0, 1.5f, 0);
     private Vector3 m_moveDirection;
     private Quaternion m_pSpawnRotation = new Quaternion(0, 0, 0, 0);
     private Rigidbody m_RB;
     private Transform m_orientation;
+
 
     void Start()
     {
@@ -59,6 +65,10 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
+        //Updating speed values
+        SpeedVertical += Time.deltaTime * AccellerationVertical;
+        SpeedHorizontal += Time.deltaTime * AccellerationHorizontal;
+
         if (Input.GetKeyDown(KeyCode.Space) && m_pIsGrounded)
         {
             m_RB.AddForce(transform.up * JumpForce, ForceMode.Impulse);
@@ -73,19 +83,13 @@ public class PlayerController : MonoBehaviour
 
     private void Movement()
     {
-        //Setting up keyInputs and movement speed
-        //Ignoring vertical movement
+        //Setting up keyInputs, Ignoring vertical movement inputs
         Vector3 keyInput = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 
-        //Horizontal Movement by player
         m_moveDirection =  m_orientation.right * keyInput.x;
-        m_RB.AddForce(m_moveDirection * PlayerMovementSpeed * SpeedMultiplier, ForceMode.Force);
+        //Apply user input horizontal movement and apply continous vertical movement
+        m_RB.velocity = m_moveDirection * SpeedHorizontal + m_orientation.forward * SpeedVertical;
 
-        //Vertical Movement by game
-        //you could move the object by continuously changing the transform, however that's more
-        //like "teleporting" the player and may result in issues with collision detection
-        m_RB.velocity = m_orientation.forward * PlayerMovementSpeed * Time.deltaTime;
-        Debug.Log(m_RB.velocity);
     }
 
     private void OnCollisionEnter(Collision collision)
